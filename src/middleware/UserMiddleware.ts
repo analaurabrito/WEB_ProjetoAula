5d54465eec2241a22da7d9347bf32a97fd77bfd7
Aura <111578898+analaurabrito@users.noreply.github.com>
2024-10-08 10:36:36 -0300

    Update UserMiddleware.ts

--- src/middleware/UserMiddleware.ts
+++ src/middleware/UserMiddleware.ts
@@ -6,7 +6,7 @@ class UserMiddleware{
     }
 
     async analyseToken(req: Request, res: Response, next: NextFunction){
-        const token = req.headers{"authorization"};
+        const token = req.headers["authorization"];
 
         if(!token){
             return res.status(401).json({
